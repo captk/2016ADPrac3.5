cf3e84c5112098e44edd7841095ebac7f58cbd33
k <k@debian>
2016-04-11 02:35:03 +0930

    Completed checkpoint 23.

--- 2016ADPrac3Patient/src/Practical3Patient/Driver.java
+++ 2016ADPrac3Patient/src/Practical3Patient/Driver.java
@@ -6,6 +6,7 @@
 package Practical3Patient;
 
 import java.util.ArrayDeque;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 /**
@@ -15,13 +16,13 @@ import java.util.Queue;
 public class Driver {
 
     public static void main(String[] args) {
-        Queue<Patient> pQueue = new ArrayDeque<Patient>();
+        Queue<Patient> pQueue = new PriorityQueue<Patient>();
         pQueue.add(new Patient("John", 5, 10.5));
         pQueue.add(new Patient("David", 10, 11.3));
         pQueue.add(new Patient("Jim", 10, 10.8));
         pQueue.add(new Patient("Joan", 3, 12.0));
         pQueue.add(new Patient("Jane", 15, 3.2));
-        System.out.println(pQueue);
+        //System.out.println(pQueue);
         
         while(!pQueue.isEmpty()){
             System.out.println(pQueue.poll());
