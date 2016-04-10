ee6d125a52439d5aab1e55041101028ae79505b9
k <k@debian>
2016-04-11 02:26:59 +0930

    Completed checkpoint 22.

---
+++ 2016ADPrac3Patient/src/Practical3Patient/Driver.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practical3Patient;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ *
+ * @author k
+ */
+public class Driver {
+
+    public static void main(String[] args) {
+        Queue<Patient> pQueue = new ArrayDeque<Patient>();
+        pQueue.add(new Patient("John", 5, 10.5));
+        pQueue.add(new Patient("David", 10, 11.3));
+        pQueue.add(new Patient("Jim", 10, 10.8));
+        pQueue.add(new Patient("Joan", 3, 12.0));
+        pQueue.add(new Patient("Jane", 15, 3.2));
+        System.out.println(pQueue);
+        
+        while(!pQueue.isEmpty()){
+            System.out.println(pQueue.poll());
+        }
+    }
+}
